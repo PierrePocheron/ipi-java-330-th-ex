f9daa59db775f9c23ff150ade326f5676b5fba0e
PierrePocheron <pierre.pocheron@gmail.com>
2020-12-01 16:58:37 +0100

    save

--- src/main/java/com/ipiecoles/java/java320/controller/EmployeController.java
+++ src/main/java/com/ipiecoles/java/java320/controller/EmployeController.java
@@ -1,7 +1,12 @@
 package com.ipiecoles.java.java320.controller;
 
+import com.ipiecoles.java.java320.model.Commercial;
 import com.ipiecoles.java.java320.model.Employe;
+import com.ipiecoles.java.java320.model.Technicien;
+import com.ipiecoles.java.java320.model.Manager;
+import com.ipiecoles.java.java320.repository.CommercialRepository;
 import com.ipiecoles.java.java320.repository.EmployeRepository;
+import com.ipiecoles.java.java320.repository.ManagerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -13,6 +18,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.Optional;
@@ -25,49 +31,93 @@ public class EmployeController {
     @Autowired
     private EmployeRepository employeRepository;
 
-
-    @RequestMapping(method = RequestMethod.GET, value ="/{id}")
+    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
     public String getEmployeById(@PathVariable Long id, final ModelMap model){
 
-        Optional<Employe> employeOptionnal = employeRepository.findById(id);
-        if(employeOptionnal.isEmpty()){
-            //Erreur 404
-            throw new EntityNotFoundException("L'employé d'identifiant : " + id + " n'a pas été trouvé.");
+        Optional<Employe> employeOptional = employeRepository.findById(id);
+        //Ici il faudrait gérer l'erreur 404 !
+        if(employeOptional.isEmpty()){
+            throw new EntityNotFoundException("L'employé d'identifiant " + id + " n'a pas été trouvé !");
         }
 
-        model.put("employe",employeOptionnal.get());
+        model.put("employe", employeOptional.get());
+        //model.put("nombreEmployes", employeRepository.count());
         return "detail";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/new/{typeEmploye}")
+    public String newEmploye(@PathVariable String typeEmploye, final ModelMap model){
+        switch (typeEmploye.toLowerCase()){
+            case "technicien": model.put("employe", new Technicien()); break;
+            case "commercial": model.put("employe", new Commercial()); break;
+            case "manager": model.put("employe", new Manager()); break;
+        }
+        return "detail";
+    }
+
+    ///employes/commercial
+    ///employes/technicien
+    ///employes/manager
+    @RequestMapping(method = RequestMethod.POST, value = "/commercial", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public RedirectView createOrSaveCommercial(Commercial employe){
+        return saveEmploye(employe);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/technicien", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public RedirectView createOrSaveTechnicien(Technicien employe){
+        return saveEmploye(employe);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/manager", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public RedirectView createOrSaveManager(Manager employe){
+        return saveEmploye(employe);
+    }
+
+    private RedirectView saveEmploye(Employe employe){
+        employe = employeRepository.save(employe);
+        return new RedirectView("/employes/" + employe.getId());
     }
 
 
 
-    @RequestMapping(value = "", method = RequestMethod.GET, params = "matricule")
-    public String findByMatricule(@RequestParam String matricule, final ModelMap model){
+
+    @RequestMapping(value = "/{id}/delete", method = RequestMethod.GET)
+    public RedirectView deleteEmploye(@PathVariable Long id){
+        if(!employeRepository.existsById(id)){
+            throw new EntityNotFoundException("L'employé d'identifiant " + id + " n'a pas été trouvé !");
+        }
+        employeRepository.deleteById(id);
+        return new RedirectView("/employes");
+    }
+
+
+
+    @RequestMapping(method = RequestMethod.GET, value = "", params = "matricule")
+    public String searchEmployeByMatricule(@RequestParam String matricule, final ModelMap model){
         Employe employe = employeRepository.findByMatricule(matricule);
-        //404
-//        if(employe == null){
-//            throw new EntityNotFoundException("L'Employé de matricule : " + matricule + " n'a pas été trouvé.");
-//        }
-        model.put("employe",employe);
+        //Ici il faudrait gérer l'erreur 404 !
+        model.put("employe", employe);
         return "detail";
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "")
     public String listEmployes(final ModelMap model,
-                               @RequestParam(value="page", defaultValue = "0") Integer page,
-                               @RequestParam(value="size", defaultValue = "10") Integer size,
-                               @RequestParam(value="sortDirection", defaultValue = "ASC") String sortDirection,
-                                @RequestParam(defaultValue = "matricule") String sortProperty)
-    {
-        PageRequest pageRequest =  PageRequest.of(page, size, Sort.Direction.fromString(sortDirection), sortProperty);
+                               @RequestParam(defaultValue = "0") Integer page,
+                               @RequestParam(defaultValue = "10") Integer size,
+                               @RequestParam(defaultValue = "matricule") String sortDirection,
+                               @RequestParam(defaultValue = "ASC") String sortProperty){
+        PageRequest pageRequest = PageRequest.of(page, size,
+                Sort.Direction.fromString(sortDirection), sortProperty);
         Page<Employe> pageEmploye = employeRepository.findAll(pageRequest);
-
         model.put("employes", pageEmploye);
+        //pageEmploye.has
+//        pageEmploye.getTotalElements();
+        //employes.totalElements =>
         model.put("pageNumber", page + 1);
         model.put("previousPage", page - 1);
         model.put("nextPage", page + 1);
         model.put("start", page * size + 1);
-        model.put("end", page + 1 * size);
+        model.put("end", (page) * size + pageEmploye.getNumberOfElements());
         return "listeEmployes";
     }
 
--- src/main/java/com/ipiecoles/java/java320/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ipiecoles/java/java320/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.persistence.EntityExistsException;
 import javax.persistence.EntityNotFoundException;
@@ -13,45 +14,15 @@ import javax.persistence.EntityNotFoundException;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    //Dans le controller mettre :
-    // throw new EntityNotFoundException("Le véhicule d'identifiant : " + id + "n'a pas été trouvé.");
-
     @ExceptionHandler(EntityNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleEntityNotFoundException(EntityNotFoundException e)
+    public ModelAndView handleEntityNotFoundException(EntityNotFoundException e)
     {
-        return e.getMessage();
-    }
+        ModelAndView modelAndView = new ModelAndView("error", HttpStatus.NOT_FOUND);
+        modelAndView.addObject("error", e.getMessage());
+        modelAndView.addObject("status", HttpStatus.NOT_FOUND);
 
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleIllegalArgumentException(IllegalArgumentException e)
-    {
-        return e.getMessage();
-    }
-
-    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e)
-    {
-        return "Le paramètre " + e.getName() + " a une valeur incorrecte : " + e.getValue();
-    }
-
-    //Pour : public Page<Employe> listEmployes -> sortProperty paramètre
-    @ExceptionHandler(PropertyReferenceException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handlePropertyReferenceException(PropertyReferenceException e)
-    {
-        return "La propriété : " + e.getPropertyName() + " n'éxiste pas !";
-    }
-
-
-    @ExceptionHandler(EntityExistsException.class)
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public String handleEntityExistsException(EntityExistsException e)
-    {
-        return e.getMessage();
+        return modelAndView;
     }
 
 }
